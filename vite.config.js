23dc52eb4f73644b2befea9456963679d9bef45f
hkonda015 <hkonda015@gmail.com>
2025-01-04 19:56:12 +0000

    Initial commit from your app

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,11 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+    import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': '/src', // Alias for the source folder
-    },
-  },
-  base: '/', // Set base path to '/' for root-level deployment
-  build: {
-    outDir: 'dist', // Make sure the build output is placed in 'dist'
-  },
-});
+    export default defineConfig({
+      plugins: [react()],
+      resolve: {
+        alias: {
+          '@': '/src'
+        }
+      }
+    });
